bb4fb5583ddbc6e9e670b4dee20ee535f01d42ac
kasinenisaikrishna <kasineni.sai@gmail.com>
2025-09-03 19:49:31 +0530

    array

--- 06-array.sh
+++ 06-array.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-fruits=("apple","kiwi","orange")
+fruits=("apple" "kiwi" "orange")
 echo "first fruit is: {$fruits[0]}"
 echo "2nd fruit is: {$fruits[1]}"
 echo "3rd fruit is: {$fruits[2]}"
