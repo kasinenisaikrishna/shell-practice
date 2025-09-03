0008eb1571d21b9085b8af5bd2dcc76b208a4dbb
kasinenisaikrishna <kasineni.sai@gmail.com>
2025-09-03 19:06:33 +0530

    added variables

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+person1 = "raheem"
+person2 = "robert"
+
+echo "$person1:: Hi $person2, how are you?"
+echo "$person2:: Hello $person1. I am fine. How are you doing?"
+echo "$person1:: I am doing good $person2 what is going on?"
+echo "$person2:: I started learning shell script $person1"
