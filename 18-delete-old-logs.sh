4d001e109ae339399380888c18000011c0b5c873
kasinenisaikrishna <kasineni.sai@gmail.com>
2025-09-07 09:13:11 +0530

    set

---
+++ 18-delete-old-logs.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+source_dir=/home/ec2-user/logs
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ -d $source_dir ]
+then
+    echo -e "$source_dir $G exists $N"
+else
+    echo -e "$source_dir $R does not exists $N"
+    exit 1
+fi
+
+files=$(find $source_dir -name "*.log" -mtime +14)
+echo "files: $files"
